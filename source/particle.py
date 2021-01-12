afad6b50ca76c0c6b4c1212062a056e6d7540d39
Liam <liam.c.swin@gmail.com>
2021-01-12 18:53:25 +1100

    Created particle class

---
+++ source/particle.py
@@ -0,0 +1,6 @@
+class particle:
+    def init(self, coord, velocity, mass, radius):
+        self.coord = coord
+        self.velocity = velocity
+        self.mass = mass
+        self.radius = radius
