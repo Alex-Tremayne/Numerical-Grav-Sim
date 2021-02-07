cf2bb94d453ab352b23ef64ecf1f83e4ac910058
Alex <alexjtremayne@gmail.com>
2021-02-07 20:01:15 +1100

    Added gif output, various bug fixes
    np.array() returns an object in memory, and is passed around by reference, adding np.copy() when setting object vars fixes this

--- source/main.py
+++ source/main.py
@@ -1,6 +1,8 @@
 import particle
 import physics
 import numpy as np
+import matplotlib.pyplot as plt
+from celluloid import Camera
 
 
 def initialise(num_particles, bounds, velocity, mass, radius=1):
@@ -10,7 +12,7 @@ def initialise(num_particles, bounds, velocity, mass, radius=1):
 
 	for x in range(num_particles):
 		#print(x)
-		temp = particle.particle(np.multiply(bounds, np.random.rand(bounds.size)), velocity, mass, radius)
+		temp = particle.particle(np.multiply(bounds, np.random.rand(bounds.size)), np.copy(velocity), mass, radius)
 		particles = np.append(particles, temp)
 
 	return particles
@@ -19,14 +21,30 @@ def initialise(num_particles, bounds, velocity, mass, radius=1):
 
 
 
-particles = initialise(10, np.array([10.0, 10.0]), np.array([10.0, 10.0]), 10.0)
-print(particles.size)
+particles = initialise(100, np.array([10.0, 10.0]), np.array([0.0, 0.0]), 100.0)
+
+
 for item in particles:
 	print(item.coord)
 
+fig = plt.figure()
+camera = Camera(fig)
 
-for x in range(100): 
-	physics.eulerSemiImplicit(1,0.001,particles)
-	print("Time: " + str(x * 0.001))
-	for particle in particles:
-		print(particle.coord)
+for x in range(500): 
+	
+
+	physics.eulerSemiImplicit(1, 0.001, particles)
+
+	# Get coords
+	coords = np.asarray([item.coord for item in particles])
+	x, y = coords.T
+
+	plt.plot(x, y, "o", color="blue")
+	plt.xlim(0,10)
+	plt.ylim(0,10)
+	camera.snap()
+
+animation = camera.animate()
+ 
+#Saving the animation
+animation.save('grav_sim.gif')
--- source/physics.py
+++ source/physics.py
@@ -3,12 +3,14 @@ import numpy as np
 def eulerSemiImplicit(grav_const, time_step, particles):
 	for particle1 in range(len(particles)):
 		particles[particle1].acceleration[:] = 0
+
 		for particle2 in range(len(particles)):
 			if particle1 != particle2:
 				displacement = particles[particle2].coord - particles[particle1].coord
 				distance = np.sum(displacement**2)**0.5
 
 				particles[particle1].acceleration += grav_const * particles[particle2].mass * displacement / (distance**3)
+
 
 		particles[particle1].velocity += particles[particle1].acceleration * time_step
 		particles[particle1].coord += particles[particle1].velocity * time_step
